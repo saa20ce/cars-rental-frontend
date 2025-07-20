020f0669e240e5290472ad4dc52d01cb9cfdd711
mnogotveth <lalkapetrovich@gmail.com>
2025-07-20 12:15:35 +0700

    car page:improvements

--- components/common/Cars/[slug]/AdditionalServices.tsx
+++ components/common/Cars/[slug]/AdditionalServices.tsx
@@ -34,10 +34,14 @@ export const AdditionalServices: React.FC<AdditionalServicesProps> = ({
 				<div className='lg:text-lg mt-[14px]'>Место подачи</div>
 				<div className='flex gap-2 w-full mt-[10px]'>
 					<CustomSelect
-						options={deliveryOptions}
+						
+						options={[
+							{label:'Без подачи',value:'no_delivery'},
+							...(deliveryOptions || [])
+						]}
 						onChange={(value) => setDeliveryOption(value as string)}
-						value={deliveryOptionSelected || defaultDeliveryValue}
-						placeholder='Выберите район'
+						value={deliveryOptionSelected || undefined}
+						placeholder='Выберите место подачи'
 						listHeight={232}
 						style={{ width: '100%', height: '36px' }}
 					/>

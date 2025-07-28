3e79304dd2538887e0a34d2a0155461a039db6ea
Andrey Dyatlov <dag_freak@icloud.com>
2025-07-28 11:10:37 +0700

    final conflict resolution

--- lib/ui/common/Button/index.tsx
+++ lib/ui/common/Button/index.tsx
@@ -7,88 +7,6 @@ import type { CSSProperties, ReactNode } from 'react';
 type Variant = 'outlined' | 'transparent' | 'default';
 
 interface CustomButtonProps extends Omit<ButtonProps, 'type' | 'variant'> {
-<<<<<<< HEAD
-  variant?: Variant;
-  icon?: ReactNode;
-  children?: ReactNode;
-  style?: CSSProperties;
-  className?: string;
-}
-
-const variantStyles: Record<
-  Variant,
-  Partial<ButtonProps> & { style?: CSSProperties }
-> = {
-  outlined: {
-    type: 'default',
-    ghost: true,
-    style: {
-      height: 40,
-      width: 124,
-    },
-  },
-  transparent: {
-    style: {
-      height: 40,
-      background: 'transparent',
-      border: '1px solid transparent',
-      color: '#f6f6f6',
-    },
-  },
-  default: {
-    type: 'primary',
-    style: {
-      // height: 40,
-      background: '#3c6e71',
-      border: 'none',
-      color: '#f6f6f6',
-      // width: '100%',
-      marginTop: 20,
-    },
-  },
-};
-
-export default function CustomButton({
-  variant = 'default',
-  icon,
-  children,
-  style,
-  className,
-  ...rest
-}: CustomButtonProps) {
-  const variantProps = variantStyles[variant];
-
-  return (
-    <ConfigProvider
-      theme={{
-        components: {
-          Button: {
-            contentFontSize: 16,
-            borderRadius: 12,
-            paddingBlock: 0,
-            paddingInline: 0,
-            fontSize: 16,
-            defaultHoverColor: '#f6f6f6',
-            defaultHoverBorderColor: '#f6f6f6',
-            defaultActiveColor: '#f6f6f6',
-            defaultActiveBorderColor: '#f6f6f6',
-            fontFamily: '"lato", "lato Fallback"',
-          },
-        },
-      }}
-    >
-      <Button
-        icon={icon}
-        {...variantProps}
-        className={className}
-        style={{ lineHeight: 'normal', height: 'auto', ...variantProps.style, ...style }}
-        {...rest}
-      >
-        {children}
-      </Button>
-    </ConfigProvider>
-  );
-=======
     variant?: Variant;
     icon?: ReactNode;
     children?: ReactNode;
@@ -174,5 +92,4 @@ export default function CustomButton({
             </Button>
         </ConfigProvider>
     );
->>>>>>> feature/components-development
 }

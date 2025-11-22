ac6fd4e30312e281cfbc83b44aa7fff734c8ea72
sancho <saa20ce@gmail.com>
2025-11-22 13:10:57 +0300

    added caching reviews

--- app/reviews/page.tsx
+++ app/reviews/page.tsx
@@ -14,8 +14,8 @@ export default async function ReviewsPage() {
     const breadcrumbs = await fetchBreadcrumbs('/reviews');
 
     const [reviewsRes, lettersRes] = await Promise.all([
-        fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/reviews/?status=published`, { cache: 'no-store' }),
-       fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/thank-you-letters/`,{ cache: 'no-store' }),
+        fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/reviews/?status=published`, { cache: 'force-cache' }),
+       fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/thank-you-letters/`,{ cache: 'force-cache' }),
     ]);
 
     const reviews = await reviewsRes.json();
